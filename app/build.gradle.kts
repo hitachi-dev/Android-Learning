ae42ce7b944ead576365edd32b9a668211e48543
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2021-05-12 20:17:37 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 30
+    buildToolsVersion = "30.0.3"
+
+    defaultConfig {
+        applicationId = "com.example.androidlearning"
+        minSdk = 26
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0.0-SNAPSHOT.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+val navVersion = "2.3.5"
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.navigation:navigation-testing:$navVersion")
+}
---
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.androidlearning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private var bottomNavigation: BottomNavigationView? = null
+    private var navController: NavController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        handleNavigation()
+        createBadge()
+    }
+
+    private fun createBadge() {
+        bottomNavigation?.getOrCreateBadge(R.id.action_menu_recycleview)?.also { badge ->
+            badge.isVisible = true
+            badge.number = 99
+        }
+
+        bottomNavigation?.getOrCreateBadge(R.id.action_menu_bottomsheet)?.also { badge ->
+            badge.isVisible = true
+            badge.clearNumber()
+        }
+
+        bottomNavigation?.getOrCreateBadge(R.id.action_menu_settings)?.also { badge ->
+            badge.isVisible = true
+            badge.number = 99999
+        }
+    }
+
+    private fun handleNavigation() {
+        bottomNavigation?.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_menu_recycleview -> navController?.navigate(R.id.recycleViewFragment).let { true }
+                R.id.action_menu_bottomsheet -> navController?.navigate(R.id.bottomSheetFragment).let { true }
+                R.id.action_menu_settings -> navController?.navigate(R.id.settingsFragment).let { true }
+                else -> false
+            }
+        }
+    }
+}
